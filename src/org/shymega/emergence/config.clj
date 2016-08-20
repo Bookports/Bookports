f118e0124e4d472e1d09008efb7b1846feac6211
Dom Rodriguez <shymega@shymega.org.uk>
2016-08-20 21:44:49 +0100

    Remove config-file functions

--- src/org/shymega/emergence/config.clj
+++ src/org/shymega/emergence/config.clj
@@ -2,14 +2,3 @@
   (:require [taoensso.timbre :as log])
   (:gen-class))
 
-(defn- get-config-file-location []
-  (log/trace "Getting configuration file.")
-  (if-let [file (java.lang.System/getenv "EMERGENCE_CONFIG")]
-    file
-    (do
-      (log/fatal "Unable to get location of configuration file.")
-      (log/fatal "We cannot continue. Bailing out!")
-      (System/exit 12))))
-
-(def config-file
-  (get-config-file-location))
