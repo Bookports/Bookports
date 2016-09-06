3fed7fa3168ff34110718e53341b2768c07f1133
Dom Rodriguez <shymega@shymega.org.uk>
2016-09-06 20:49:13 +0100

    Refactor dependencies list
    - Add comments for each library
    - Remove liberator, replace w/ Ring & Compojure{-api,}
    - Add Buddy for auth
    - Remove clj-uuid
    - Remove uuid

--- project.clj
+++ project.clj
@@ -6,19 +6,20 @@
   :license {:name "Apache License v2.0"
             :url "http://www.apache.org/licenses/"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.taoensso/timbre "4.7.3"]
-                 [cheshire "5.6.3"]
-                 [clj-time "0.12.0"]
-                 [sonian/carica "1.2.1"]
-                 [liberator "0.14.1"]
-                 [com.ashafa/clutch "0.4.0"]
-                 [speclj "3.3.2"]
-                 [com.draines/postal "1.11.3"]
-                 [danlentz/clj-uuid "0.1.6"]
-                 [listora/uuid "0.1.2"]
-                 [clj-isbn "0.0.7"]
-                 [clj.qrgen "0.4.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"] ;; Core library
+                 [com.taoensso/timbre "4.7.3"] ;; Logging 
+                 [cheshire "5.6.3"] ;; JSON support
+                 [clj-time "0.12.0"] ;; Date & Time library
+                 [sonian/carica "1.2.1"] ;; Configuration library - EDN
+                 [com.ashafa/clutch "0.4.0"] ;; CouchDB library
+                 [speclj "3.3.2"] ;; Test library
+                 [com.draines/postal "1.11.3"] ;; Email library
+                 [buddy "1.1.0"] ;; For authentication
+                 [clj-isbn "0.0.7"] ;; ISBN parsing library
+                 [javax.servlet/servlet-api "2.5"] ;; Java Servlet API lib
+                 [ring "1.5.0"] ;; For web apps
+                 [compojure "1.5.1"] ;; For web apps
+                 [metosin/compojure-api "1.1.8"]] ;; For web API's
 
   :main org.shymega.emergence.core
 
